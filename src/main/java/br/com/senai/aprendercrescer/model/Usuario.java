2cf411ccbd7153a512b24faf76437af52b0b8918
Luciano <lbs.been@gmail.com>
2016-09-12 22:48:15 -0300

    message

---
+++ src/main/java/br/com/senai/aprendercrescer/model/Usuario.java
@@ -0,0 +1,70 @@
+package br.com.senai.aprendercrescer.model;
+
+import java.util.Date;
+
+public class Usuario {
+  private int idUsuario;
+  private int idgrupo;
+  private String login;
+  private String senha;
+  private String nome;
+  private char flagInativo;
+  private Date dtAltercao;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idusuario) {
+        this.idUsuario = idusuario;
+    }
+
+    public int getIdgrupo() {
+        return idgrupo;
+    }
+
+    public void setIdgrupo(int idgrupo) {
+        this.idgrupo = idgrupo;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getFlagInativo() {
+        return flagInativo;
+    }
+
+    public void setFlagInativo(char flagInativo) {
+        this.flagInativo = flagInativo;
+    }
+
+    public Date getDtAltercao() {
+        return dtAltercao;
+    }
+
+    public void setDtAltercao(Date dtAltercao) {
+        this.dtAltercao = dtAltercao;
+    }
+
+}
---
+++ src/main/java/br/com/senai/aprendercrescer/ws/UsuarioWs.java
@@ -0,0 +1,31 @@
+package br.com.senai.aprendercrescer.ws;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Path("/usuario")
+public class UsuarioWs {
+
+    @GET
+    @Path("/getusuario")
+    @Produces("application/json")
+    public Response getUsuario() {
+        try {
+            JSONObject retorno = new JSONObject();
+            retorno.put("nome", "Luciano");
+            retorno.put("idade", 29);
+            return Response.status(200).entity(retorno.toString()).build();
+        } catch (JSONException ex) {
+            Logger.getLogger(UsuarioWs.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return Response.status(500).build();
+    }
+
+}
